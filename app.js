01a24bccce1184748e572cf9e2c99c72fa521e60
nnAsaddd <142248564+nnAsaddd@users.noreply.github.com>
2023-11-22 19:22:05 +0500

    Update app.js

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const cors = require("cors");
 const port = process.env.PORT || 5000;
 const allowedOrigins = [
   "http://localhost:3000",
+  "http://localhost:5173",
   "https://futureafinds.netlify.app",
   "https://inventory-manager-frontend-nu.vercel.app"
 ];
